52d6fb869933341d074ef34e5180067284d797e0
kusham <kusham1998@gmail.com>
2022-04-04 01:35:12 +0530

    create components for create and update candidate

--- src/Pages/RecruitmentModule/CreateCandidateProfile/CreateCandidate.jsx
+++ src/Pages/RecruitmentModule/CreateCandidateProfile/CreateCandidate.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import { Grid } from "@mui/material";
+import React, { useState } from "react";
+import CreateCandidateForm from "../../../Components/RecruitmentModule/CreateCandidateForm/CreateCandidateForm";
+import SearchCandidate from "../../../Components/RecruitmentModule/SearchCandidate/SearchCandidate";
 
 const CreateCandidate = () => {
-  return <div>CreateCandidate</div>;
+  const [candidateData, setCandidateData] = useState({
+    firstName: "",
+    lastName: "",
+    NIC: "",
+    phoneNumber: "",
+    email: "",
+    cv: "",
+  });
+  const [candidateId, setCandidateId] = useState(null);
+
+  
+  return (
+    <Grid container>
+      <Grid item sm={12} md={8}>
+        <CreateCandidateForm
+          candidateData={candidateData}
+          setCandidateData={setCandidateData}
+          candidateId ={candidateId}
+        />
+      </Grid>
+      <Grid item sm={3} md={4}>
+        <SearchCandidate
+         setCandidateData={setCandidateData}
+         setCandidateId={setCandidateId}
+        />
+      </Grid>
+    </Grid>
+  );
 };
 
 export default CreateCandidate;

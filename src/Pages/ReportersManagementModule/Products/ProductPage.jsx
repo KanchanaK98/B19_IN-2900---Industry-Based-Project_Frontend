bce3346ebb24d1288c44780db8e835bd00dc34f8
Nimasha-Madhushani <nimashamadhushani19@gmail.com>
2022-05-08 11:17:22 +0530

    changes in product page

--- src/Pages/ReportersManagementModule/Products/ProductPage.jsx
+++ src/Pages/ReportersManagementModule/Products/ProductPage.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import { Grid, Button} from "@mui/material";
 import DisplayProducts from "../../../Components/ReportersManagementModule/ProductDisplay/DisplayProducts";
 import SearchProduct from "../../../Components/ReportersManagementModule/ProductSearch/SearchProduct";
-
+import {Link} from 'react-router-dom'
 function ProductPage() {
   return (
     <div>
       <Grid item sm={12} md={12} sx={{ mb: 5 }}>
         <Grid container>
           <Grid item md={6}>
-            <Button variant="contained">Create New Product</Button>
+          <Link to="/products/create">
+          <Button type="button" variant="contained">
+           Create New Product
+          </Button>
+        </Link>
           </Grid>
           <Grid item md={6}>
             {/* <SearchProduct /> */}

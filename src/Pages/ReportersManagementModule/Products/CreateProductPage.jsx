09bb63f26b7ed824918a2e4471689fb07fcdaa39
Nimasha-Madhushani <nimashamadhushani19@gmail.com>
2022-05-13 00:41:33 +0530

    changes in create product page

--- src/Pages/ReportersManagementModule/Products/CreateProductPage.jsx
+++ src/Pages/ReportersManagementModule/Products/CreateProductPage.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import CreateProduct from '../../../Components/ReportersManagementModule/ProductCreate/CreateProduct'
+import { Grid, Box } from "@mui/material";
+import React from "react";
+import Drawer from '@mui/material/Drawer';
+import ProductCountUp from "../../../Components/ReportersManagementModule/CountUps/CountUpStack";
+import CreateProduct from "../../../Components/ReportersManagementModule/ProductCreate/CreateProduct";
 
 function CreateProductPage() {
   return (
-    <div><CreateProduct/></div>
-  )
+    <div>
+      <Grid container>
+      <Grid item md={9}>
+        <Box padding={3}>
+        
+            <ProductCountUp />
+            <CreateProduct />
+        
+        </Box>
+        </Grid>
+        <Grid item md={3}>
+       
+
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
-export default CreateProductPage
+export default CreateProductPage;

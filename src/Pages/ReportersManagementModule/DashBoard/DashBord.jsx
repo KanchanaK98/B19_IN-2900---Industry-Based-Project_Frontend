4b92a014967ce916f01a52853b95c1d8ff44e3e4
Nimasha-Madhushani <nimashamadhushani19@gmail.com>
2022-05-13 00:41:08 +0530

    add a new tab to dashboard page

--- src/Pages/ReportersManagementModule/DashBoard/DashBord.jsx
+++ src/Pages/ReportersManagementModule/DashBoard/DashBord.jsx
@@ -9,6 +9,7 @@ import TabContext from "@mui/lab/TabContext";
 import TabList from "@mui/lab/TabList";
 import TabPanel from "@mui/lab/TabPanel";
 import { Link } from "react-router-dom";
+import OrganizationStructure from "../../../Components/ReportersManagementModule/OrganizationStructure/OrganizationStructure";
 function DashBord() {
   const [value, setValue] = React.useState("1");
 
@@ -33,14 +34,18 @@ function DashBord() {
                 onChange={handleChange}
                 aria-label="lab API tabs example"
               >
-                <Tab label="ORGANIZATION STRUCTURE" value="1" />
-                <Tab label="RECENT EMPLOYEE SECTION" value="2" />
+                  <Tab label="ORGANIZATION STRUCTURE" value="1" />
+                <Tab label="ALL EMPLOYEES" value="2" />
+                <Tab label="RECENT EMPLOYEE SECTION" value="3" />
               </TabList>
             </Box>
             <TabPanel value="1" sx={{ mt: 3}}>
+            <OrganizationStructure/>
+            </TabPanel>
+            <TabPanel value="2" sx={{ mt: 3}}>
               <DisplayProfiles />
             </TabPanel>
-            <TabPanel value="2" sx={{ mt: 7 }}>
+            <TabPanel value="3" sx={{ mt: 7 }}>
               <AllRecentEmployees />
             </TabPanel>
           </TabContext>

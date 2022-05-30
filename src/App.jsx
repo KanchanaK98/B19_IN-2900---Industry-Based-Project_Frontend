505ddd17d46e7421b216ddc3daaec830d50f5190
Nimasha-Madhushani <nimashamadhushani19@gmail.com>
2022-05-30 22:14:26 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -37,6 +37,7 @@ import ViewAllPapersDelete from "./Pages/PromotionModule/Paper/DisplayPaperAndDe
 import CreateNewPaper from "./Pages/PromotionModule/Paper/CreatePaper";
 import AllSubmissions from "./Pages/PromotionModule/AllSubmissions/AllSubmissions";
 import DisplayTeamMemberSubmissions from "./Pages/PromotionModule/Evaluations/TeamLead/DisplayTeamSubmissionsAndFeedback";
+import JobRoleDialogBox from "./Components/ReportersManagementModule/CreateEmployeeForm/JobRoleDialogBox";
 
 function App() {
   const [open, setOpen] = useState(true);
@@ -79,7 +80,7 @@ function App() {
               <Route path="/products/update/:id" element={<EditProduct />} />
               <Route path="progress" element={<ProgressBar />} />
               <Route path="tree" element={<CustomizedTeamView />} />
-              
+              <Route path="job" element={<JobRoleDialogBox/>}/>
 
               {/* Recruitment management */}
               <Route path="/candidate" element={<CreateCandidate />} />

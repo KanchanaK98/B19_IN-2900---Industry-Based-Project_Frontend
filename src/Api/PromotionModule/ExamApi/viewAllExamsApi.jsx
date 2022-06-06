2de93307fd82451269e58d2d27659497c4ba6660
IKNManimendra <manimendraikn.19@uom.lk>
2022-06-06 17:32:06 +0530

    minor change

--- src/Api/PromotionModule/ExamApi/viewAllExamsApi.jsx
+++ src/Api/PromotionModule/ExamApi/viewAllExamsApi.jsx
@@ -1,9 +1,9 @@
 import * as api from "../../index";
 
-export const viewAllExamsApi = async () => {
+export const viewAllExamsApi = async (EmployeeID) => {
   try {
-    const { data } = await api.viewAllExams();
-    console.log("viewAllExamsApi executed");
+    const { data } = await api.viewAllExams(EmployeeID);
+    //console.log("viewAllExamsApi executed");
     return data;
   } catch (error) {
     console.log(error);

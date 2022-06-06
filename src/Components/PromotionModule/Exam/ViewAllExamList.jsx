f1a7d15fe4e9ca1410fd6efefc0625c0a90602a8
IKNManimendra <manimendraikn.19@uom.lk>
2022-06-06 17:38:22 +0530

    create table of exams, added shedule button

--- src/Components/PromotionModule/Exam/ViewAllExamList.jsx
+++ src/Components/PromotionModule/Exam/ViewAllExamList.jsx
@@ -1,16 +1,97 @@
 import { useState, useEffect } from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { Button } from "@mui/material/";
+import { useParams } from "react-router-dom";
 import { viewAllExamsApi } from "../../../Api/PromotionModule/ExamApi/viewAllExamsApi";
 
 const ViewAllExamList = () => {
-  const [PaperList, setPaperList] = useState([]);
+  const [ExamsList, setExamsList] = useState([]);
+
+  const { EmployeeID } = useParams();
+  console.log(EmployeeID);
 
   useEffect(() => {
     async function fetchData() {
-      setPaperList(await viewAllExamsApi());
+      setExamsList(await viewAllExamsApi());
     }
     fetchData();
   }, []);
-  console.log(PaperList);
-  return <div>from all exm list</div>;
+  console.log(ExamsList);
+  return (
+    <div>
+      <Button
+        variant="contained"
+        sx={{ backgroundColor: "#183d78" }}
+        onClick={() =>
+          window.open(
+            ` /promotion/evaluation/exam/scheduleExam/${EmployeeID}`,
+            "_self"
+          )
+        }
+      >
+        Schedule New Exam&nbsp;
+      </Button>
+      <h1>All Exams</h1>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>ExamID</TableCell>
+              <TableCell align="right">ExamName</TableCell>
+              <TableCell align="right">DateScheduled</TableCell>
+              <TableCell align="right">JobRole</TableCell>
+              <TableCell align="right">PaperID</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {ExamsList.map((element, key) => (
+              <TableRow
+                key={key}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {element.ExamID}
+                </TableCell>
+                <TableCell align="right">{element.ExamName}</TableCell>
+                <TableCell align="right">{element.DateScheduled}</TableCell>
+                <TableCell align="right">{element.JobRole}</TableCell>
+                <TableCell align="right">{element.PaperID}</TableCell>
+                <Button
+                  variant="contained"
+                  sx={{ backgroundColor: "#183d78" }}
+                  //   onClick={() =>
+                  //     window.open(
+                  //       ` /promotion/evaluation/exam/scheduleExam/${EmployeeID}`,
+                  //       "_self"
+                  //     )
+                  //   }
+                >
+                  Update&nbsp;
+                </Button>{" "}
+                <Button
+                  variant="contained"
+                  sx={{ backgroundColor: "#183d78" }}
+                  //   onClick={() =>
+                  //     window.open(
+                  //       ` /promotion/evaluation/exam/scheduleExam/${EmployeeID}`,
+                  //       "_self"
+                  //     )
+                  //   }
+                >
+                  Delete&nbsp;
+                </Button>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
 };
 export default ViewAllExamList;

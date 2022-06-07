d900078a4f143e2270b1e9fba4fc8f2330a16796
Nimasha-Madhushani <nimashamadhushani19@gmail.com>
2022-06-07 12:07:41 +0530

    changes in recent section components

--- src/Components/ReportersManagementModule/RecentSection/AllRecentEmployees.jsx
+++ src/Components/ReportersManagementModule/RecentSection/AllRecentEmployees.jsx
@@ -1,46 +1,46 @@
 import { useState, useEffect, React } from "react";
 import { Box, Grid } from "@mui/material";
-// import useStyles from "./RecentEmployeeSectionStyles";
 import RecentEmployee from "./RecentEmployee";
 import { recentEmployees } from "../../../Api/ReportersManagementModule/EmployeeApi";
 
+// import Carousel from '@brainhubeu/react-carousel';
+// import '@brainhubeu/react-carousel/lib/style.css';
+
 function AllRecentEmployees() {
   const [profiles, setProfiles] = useState([]);
-  // console.log(profiles)
+
   useEffect(() => {
     async function fetchData() {
       setProfiles(await recentEmployees());
     }
     fetchData();
- 
   }, []);
 
-  // console.log(profiles);
-  // const classes = useStyles();
   return (
     <div>
-      <Box maxWidth={400} >
-      <Grid
-        container
-        spacing={4}
-        sx={{
-         
-          justifyContent: "center",
-          display: "flex",
-        }}
-      >
-        {profiles &&
-          profiles.map((prof) => {
-            return (
-              <div  key={prof._id}>
-                <Grid item xs={12} sm={6} md={4} component="span">
-                  <RecentEmployee profile={prof} />
-                </Grid>
-              </div>
-            );
-          })}
-      </Grid>
+        {/* <Carousel> */}
+      <Box maxWidth={400}>
+        <Grid
+          container
+          spacing={4}
+          sx={{
+            justifyContent: "center",
+            display: "flex",
+          }}
+        >
+          {profiles &&
+            profiles.map((prof) => {
+              return (
+                <div key={prof._id}>
+                  <Grid item xs={12} sm={6} md={4} component="span" >
+                    <RecentEmployee profile={prof} />
+                  </Grid>
+                </div>
+              );
+            })}
+        </Grid>
       </Box>
+      {/* </Carousel> */}
     </div>
   );
 }

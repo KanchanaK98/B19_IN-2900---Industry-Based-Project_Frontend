dac6a910c9cf10e1c31badf33ffc3cbf3a30d17d
Nimasha-Madhushani <nimashamadhushani19@gmail.com>
2022-06-14 13:26:45 +0530

    adding snackbar to show when entering duplicated data

--- src/Pages/ReportersManagementModule/DashBoard/DisplayAllEmployees.jsx
+++ src/Pages/ReportersManagementModule/DashBoard/DisplayAllEmployees.jsx
@@ -1,29 +1,18 @@
 import { Box, Grid } from "@mui/material";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import AllEmployees from "../../../Components/ReportersManagementModule/DisplayEmployees/AllEmployees";
 import DisplayProfile from "../../../Components/ReportersManagementModule/DisplayEmployees/DisplayProfile";
 
 function DisplayAllEmployees({ profiles }) {
-  // const [profiles, setProfiles] = useState([]);
   const [employee, setEmployee] = useState(profiles[0]);
-  console.log(profiles);
-  console.log(employee);
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     setProfiles(await viewAllEmployees());
-  //     setEmployee( profiles[0]);
-  //    console.log("hi 1")
-  //   }
-  //   fetchData();
-  // }, []);
-  // console.log("Hi 2");
+
   return (
-    <Box sx={{backgroundColor:"#d7dde0",padding:2}}>
+    <Box sx={{ backgroundColor: "#d7dde0", padding: 2 }}>
       <Grid container>
         <Grid item md={4} xs={12}>
-          <AllEmployees setEmployee={setEmployee} profiles={profiles}/>
+          <AllEmployees setEmployee={setEmployee} profiles={profiles} />
         </Grid>
-        <Grid item md={8} xs={12} sx={{padding:1}}>
+        <Grid item md={8} xs={12} sx={{ padding: 1 }}>
           {employee && <DisplayProfile employee={employee} />}
         </Grid>
       </Grid>
